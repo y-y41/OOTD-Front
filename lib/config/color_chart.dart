30b94c53bae1ace1f6dabd8925ef364d1943aa2f
y-y41 <storyyun03@naver.com>
2025-01-04 15:17:09 +0900

    appbar, bottombar initial setting

---
+++ lib/config/color_chart.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class ColorChart {
+  static const Color ootdGreen = Color(0xFFC0E1AE);
+  static const Color ootdIvory = Color(0xFFFFFAF3);
+  static const Color ootdTextGrey = Color(0xFFB3B3B3);
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,1 +1,72 @@
-st
+import 'package:flutter/material.dart';
+import 'package:madcamp_w2/config/color_chart.dart';
+import 'package:madcamp_w2/screens/today_screen.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<Widget> pages = [Text('sub1'), TodayScreen(), Text('sub2')];
+  var index = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: ColorChart.ootdIvory,
+        title: Padding(
+          padding: EdgeInsets.only(left: 30),
+          child: Transform.scale(
+            scaleX: 1.4,
+            child: Text(
+              'OOTD',
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 40,
+                  fontWeight: FontWeight.w800),
+            ),
+          ),
+        ),
+      ),
+      body: Container(
+        color: ColorChart.ootdIvory,
+        padding: EdgeInsets.all(20),
+        child: pages[index],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Colors.white,
+          currentIndex: index,
+          onTap: (value) {
+            setState(() {
+              index = value;
+            });
+          },
+          unselectedItemColor: ColorChart.ootdTextGrey,
+          selectedItemColor: ColorChart.ootdGreen,
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.calendar_month_rounded,
+                  size: 30,
+                ),
+                label: '주간'),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.add,
+                  size: 30,
+                ),
+                label: '오늘'),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.search_rounded,
+                  size: 30,
+                ),
+                label: '검색'),
+          ]),
+    );
+  }
+}
